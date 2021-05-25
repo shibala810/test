f0b060ae9dc8f8c1a27130529cbb1b7f43b9ea61
shibala810 <shibala810@gmail.com>
2021-05-25 12:20:07 +0800

    file commit

--- fileread.py
+++ fileread.py
@@ -1,7 +1,10 @@
 data = []
 
-with open('food.txt','r') as f:
+count = 0
+with open('reviews.txt','r') as f:
     for line in f:
         data.append(line.strip())
-print(len(data))
-print(data)
+        count +=1
+        if count % 1000 ==0:
+            print(count)
+print('檔案讀取完畢，共有',len(data),'筆資料')
